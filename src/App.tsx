dd07af1d1952bfb9810fffef12b2ee44347d5548
Giga Todua <89189706+Todu66@users.noreply.github.com>
2023-10-03 20:56:06 +0400

    installing git

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Hero from "./components/Hero";
+// import ApiRequest from './components/ApiRequest';
+import "./index.css"
+function App() {
+  
+
+  return (
+    <div className="">
+      <Hero />
+      {/* <ApiRequest /> */}
+    </div>
+  )
+}
+
+export default App
